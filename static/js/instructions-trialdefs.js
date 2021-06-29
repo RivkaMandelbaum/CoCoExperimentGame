856b7efb1da5a280795eef030b9fb09308d08dd2
Rivka Mandelbaum <rivkam@princeton.edu>
2021-06-29 10:16:21 -0400

    Improved removing participants from study for failing attention checks

--- static/js/instructions-trialdefs.js
+++ static/js/instructions-trialdefs.js
@@ -8,68 +8,81 @@ const TRAINING_NUM_DECISIONS = 2;
 
 const practice_explanation = "<p id='practice-explanation'>This is a practice round. Your choices in this round do <strong>not</strong> impact your final bonus.</p>";
 
+function createNodeWithTrial(trial_definition) { 
+    const NODE_TEMPLATE = {
+        timeline: null,
+        conditional_function: function() { return isValidPlayer(); },
+    };
+
+    let customNode = Object.assign({}, NODE_TEMPLATE);
+    customNode.timeline = [trial_definition];
+    return customNode;
+}
+
 /* ---- pages of text to introduce the game ---- */
 let game_goal = "<div id='game-goal'>placeholder</div>";
 let copy_fee = "<div id='copy-fee'>placeholder</div>";
 
-let instructions_explanation = {
-    timeline: [
-        {
-            type: "instructions",
-            on_start: function() { 
-                intervalID = startTimer(TRAINING_TRIAL_DURATION/ 1000);
-                document.getElementById("money-amount").innerHTML = "Your total amount of money is: " + START_MONEY.toString();
-            },
-            pages: ["<div id = 'instructions-welcome'><h1>Welcome to the game of Art Connoisseur!</h1></div>"],
-            show_clickable_nav: true,            
-            allow_backward: false,
-            on_finish: function() { 
-                clearInterval(intervalID);
-            },
-        },
-        {
-            type: "instructions",
-            on_start: function() { 
-                intervalID = startTimer(TRAINING_TRIAL_DURATION * 3 / 1000);
-            },
-            show_clickable_nav: true,
-            allow_backward: false,
-            pages: [
-                game_goal,
-                "<div id = 'art-values'><h1>The Arts and Their Values</h1><p>The artwork values can be $1, $2, $3, $4, $5, $6, $7, $8, $9, or $10.</p><p>The value of the artwork you selected will become your bonus.</p><p>That is, if the art you selected is worth $1, you earn 1 cent; if it is worth $10, you earn 10 cents.</div>",
-                "<div id= 'copy-explanation'><h1>Learn From an Art Connoisseur</h1><p>You will see how you and others did after each round.</p><p>In each round you can either select the artwork on your own, or you can learn from others.</p><p>If you think there is a player who is really good at the game, you can choose to copy their selections.</p></div>",
-        
-            ],
-            // the game only accesses these constants during actual gameplay
-            on_load: function() { 
-                document.getElementById("game-goal").innerHTML = `<div id='game-goal-wrapper'><h1>Choose Arts - Earn Bonus</h1><p>Imagine you are invited to an art museum with ${numPlayers} other players.</p><p>In each round, all players will be shown ${NUM_IMAGES} randomly selected artworks. </p><p>You will be asked to choose the art that you think is the most expensive.</p></div>`;
-            },
-            on_finish: function() { 
-                clearInterval(intervalID);
-            }
-        },
-        {
-            type: "instructions",
-            on_start: function() { 
-                intervalID = startTimer(TRAINING_TRIAL_DURATION * 3 / 1000);
-            },
-            show_clickable_nav: true,
-            allow_backward: false,
-            pages: [
-                copy_fee,
-                "<div id='time'><h1>Let's Keep It in Time</h1><p>Please complete your decision within the designated time at each round.</p><p>If you do not finish in time, your choice will be made for you, and if you run out of time in three rounds, you will be removed from the game.</p><p>Your total amount of money earned can be found on the bottom right of the screen.</p></div>",
-                "<div id = 'practice'><h1>Practice</h1><p>We hope you will enjoy this game!</p><p>Let's first practice a little bit.</p><p>Press next to begin practice.</p></div>",
-            ],
-            // the game only accesses these constants during actual gameplay
-            on_load: function() { 
-                document.getElementById("copy-fee").innerHTML = `<div id='copy-fee-wrapper'><h1>Pay $${COPY_FEE} to Learn From Others</h1><p>If you decide to copy, all you need to do is pay $1 (worth 1 real cent) to that player.</p><p>If you copy someone, you receive the same reward as they do.</p><p>If you copy, you will be able to see the artworks, but you will not select on your own.</p></div>`;
-            },
-            on_finish: function() { 
-                clearInterval(intervalID);
-            }
-        }]
-    }
+let instructions_node_1 = createNodeWithTrial({
+    type: "instructions",
+    on_start: function() { 
+        intervalID = startTimer(TRAINING_TRIAL_DURATION/ 1000);
+        document.getElementById("money-amount").innerHTML = "Your total amount of money is: " + START_MONEY.toString();
+    },
+    pages: ["<div id = 'instructions-welcome'><h1>Welcome to the game of Art Connoisseur!</h1></div>"],
+    show_clickable_nav: true,            
+    allow_backward: false,
+    on_finish: function() { 
+        clearInterval(intervalID);
+    },
+});
 
+let instructions_node_2 = createNodeWithTrial({
+    type: "instructions",
+    on_start: function() { 
+        intervalID = startTimer(TRAINING_TRIAL_DURATION * 3 / 1000);
+    },
+    show_clickable_nav: true,
+    allow_backward: false,
+    pages: [
+        game_goal,
+        "<div id = 'art-values'><h1>The Arts and Their Values</h1><p>The artwork values can be $1, $2, $3, $4, $5, $6, $7, $8, $9, or $10.</p><p>The value of the artwork you selected will become your bonus.</p><p>That is, if the art you selected is worth $1, you earn 1 cent; if it is worth $10, you earn 10 cents.</div>",
+        "<div id= 'copy-explanation'><h1>Learn From an Art Connoisseur</h1><p>You will see how you and others did after each round.</p><p>In each round you can either select the artwork on your own, or you can learn from others.</p><p>If you think there is a player who is really good at the game, you can choose to copy their selections.</p></div>",
+
+    ],
+    // the game only accesses these constants during actual gameplay
+    on_load: function() { 
+        document.getElementById("game-goal").innerHTML = `<div id='game-goal-wrapper'><h1>Choose Arts - Earn Bonus</h1><p>Imagine you are invited to an art museum with ${numPlayers} other players.</p><p>In each round, all players will be shown ${NUM_IMAGES} randomly selected artworks. </p><p>You will be asked to choose the art that you think is the most expensive.</p></div>`;
+    },
+    on_finish: function() { 
+        clearInterval(intervalID);
+    }
+});
+
+let instructions_node_3 = createNodeWithTrial({
+    type: "instructions",
+    on_start: function() { 
+        intervalID = startTimer(TRAINING_TRIAL_DURATION * 3 / 1000);
+    },
+    show_clickable_nav: true,
+    allow_backward: false,
+    pages: [
+        copy_fee,
+        "<div id='time'><h1>Let's Keep It in Time</h1><p>Please complete your decision within the designated time at each round.</p><p>If you do not finish in time, your choice will be made for you, and if you run out of time in three rounds, you will be removed from the game.</p><p>Your total amount of money earned can be found on the bottom right of the screen.</p></div>",
+        "<div id = 'practice'><h1>Practice</h1><p>We hope you will enjoy this game!</p><p>Let's first practice a little bit.</p><p>Press next to begin practice.</p></div>",
+    ],
+    // the game only accesses these constants during actual gameplay
+    on_load: function() { 
+        document.getElementById("copy-fee").innerHTML = `<div id='copy-fee-wrapper'><h1>Pay $${COPY_FEE} to Learn From Others</h1><p>If you decide to copy, all you need to do is pay $1 (worth 1 real cent) to that player.</p><p>If you copy someone, you receive the same reward as they do.</p><p>If you copy, you will be able to see the artworks, but you will not select on your own.</p></div>`;
+    },
+    on_finish: function() { 
+        clearInterval(intervalID);
+    }
+});
+
+let instructions_explanation = {
+    timeline: [instructions_node_1, instructions_node_2, instructions_node_3]
+};
 
 /* ---- experiment setup timeline ---- */ 
 // functionality to perform after players have all joined
@@ -111,10 +124,10 @@ let startWait = {
     function_ends_trial: true,  
 }
 
-let startTrial = [startWait];
+let startTrial = [createNodeWithTrial(startWait)];
 
 /* --- trials to learn the mechanisms of the game --- */
-let intro_mechanism_trial = {
+let intro_mechanism_trial = createNodeWithTrial({
     type: "html-keyboard-response",
     stimulus: function() { 
         return `<h1>Welcome!</h1><p>To other players, you will appear as: <div id='player-name-avatar'><img src=${player.avatar_filepath} id ='intro-player-avatar' />${player.name.replace("(you)", "")}</div>`;
@@ -128,12 +141,12 @@ let intro_mechanism_trial = {
     on_finish: function() { 
         clearInterval(intervalID);
     }
-}
+});
 
 let attempts_first = 0; 
 
 /* first practice round: showing images */
-let first_mechanism_trial = { 
+let first_mechanism_trial = createNodeWithTrial({ 
     type: "multi-image-button-response", 
     on_start: function() { 
         intervalID = startTimer(TRAINING_TRIAL_DURATION / 1000);
@@ -155,7 +168,7 @@ let first_mechanism_trial = {
         
         if(was_wrong){
             attempts_first++;
-            if (attempts_first > 2) failedAttentionCheck = true;
+            if (attempts_first >= 2) failedAttentionCheck = true;
 
             btn_html = [];
             for(i = 0; i < NUM_IMAGES; i++) { 
@@ -187,20 +200,20 @@ let first_mechanism_trial = {
             dummyPlayers[i].reward += IMG1.value;
         }
     }, 
-}
+});
 
 let first_mechanism_timeline = {
     timeline: [first_mechanism_trial],
     loop_function: function(data) { 
         // return true when it should loop
-        return (data.values()[0].button_pressed !== 0);
+        return (isValidPlayer() && (data.values()[0].button_pressed !== 0));
     }
 }
 
 let correct_player = -1;
 let attempts_second = 0;
 
-let second_mechanism_round = { 
+let second_mechanism_round = createNodeWithTrial({ 
     type: "html-button-response",
     on_start: function() { 
         intervalID = startTimer(TRAINING_TRIAL_DURATION / 1000);
@@ -241,7 +254,7 @@ let second_mechanism_round = {
         
         if(was_wrong){
             attempts_second++;
-            if (attempts_second > 2) failedAttentionCheck = true;
+            if (attempts_second >= 2) failedAttentionCheck = true;
 
             btn_html = [];
             for(i = 0; i < NUM_IMAGES; i++) { 
@@ -263,18 +276,18 @@ let second_mechanism_round = {
     on_finish: function() { 
         clearInterval(intervalID);
     },
-};
+});
 
 let second_mechanism_timeline = {
     timeline: [second_mechanism_round],
     loop_function: function(data) { 
-        return(data.values()[0].button_pressed !== (correct_player+1));
+        return(isValidPlayer() && (data.values()[0].button_pressed !== (correct_player+1)));
     }
 }
 
 let mechanism_rounds = [intro_mechanism_trial, first_mechanism_timeline, second_mechanism_timeline];
 
-let transition_screen = {
+let transition_screen = createNodeWithTrial({
     type: "html-button-response", 
     stimulus: "Nice job! You'll play a few more practice rounds, then answer some questions before moving to the real game.",
     choices: ["Continue"],
@@ -285,11 +298,11 @@ let transition_screen = {
         clearInterval(intervalID);
     }
 
-}
+});
 
 let instructions_artDisplaySelectionChoice = Object.assign({}, artDisplaySelectionChoice);
 instructions_artDisplaySelectionChoice.preamble = practice_explanation;
-let instructions_artDisplaySelection = [instructions_artDisplaySelectionChoice, artDisplaySelectionWait];
+let instructions_artDisplaySelection = [createNodeWithTrial(instructions_artDisplaySelectionChoice), createNodeWithTrial(artDisplaySelectionWait)];
 
 let instructions_artDisplayCopyChoice = Object.assign({}, artDisplayCopyChoice);
 instructions_artDisplayCopyChoice.preamble =  function() { 
@@ -302,32 +315,32 @@ instructions_artDisplayCopyChoice.preamble =  function() {
         console.warn("Art display copy trial reached, but playerState.is_copying is false!");
     }
 }
-let instructions_artDisplayCopy = [instructions_artDisplayCopyChoice, artDisplayCopyWait];
+let instructions_artDisplayCopy = [createNodeWithTrial(instructions_artDisplayCopyChoice), createNodeWithTrial(artDisplayCopyWait)];
 
 let realistic_training_trials = {
     timeline: [
 			// if not copying: display art and allow selection; update money of all players
-			{
+			createNodeWithTrial({
                 // timeline: instructions_artDisplaySelection, 
                 timeline: instructions_artDisplaySelection,
 				conditional_function: function() { 
 					// return true when the player will select 
 					return !playerState.is_copying;  
                 }, 
-			},
+			}),
 			// if copying: display art and disallow selection; update money of all players 
-			{
+			createNodeWithTrial({
 				timeline: instructions_artDisplayCopy, 
 				conditional_function: function() { 
 					// return true when the player does not select
 					return playerState.is_copying; 
 				}  
-			},
+			}),
             // display player's results and allow choice to copy (or "continue" in last round)
             // using TRAINING_NUM_DECISIONS, so not the same as trialdefs version
-			{
+			createNodeWithTrial({
                 timeline: [
-                {
+                createNodeWithTrial({
                     type: "html-button-response",
                     on_start: function() { 
                         intervalID = startTimer(TRAINING_TRIAL_DURATION / 1000);
@@ -375,14 +388,14 @@ let realistic_training_trials = {
                     },
                     response_ends_trial: true,
                     trial_duration: TRAINING_TRIAL_DURATION
-                }],
+                })],
 
                 conditional_function: function() { 
                     return numExecutions < TRAINING_NUM_DECISIONS;
                 }
-			},
+			}),
 			// update stats for all players, playerState.is_copying, playerState.player_copying_id if copying (except last round)
-			{
+			createNodeWithTrial({
                 timeline: [chooseToCopyWait],
                 conditional_function: function() {
 					let is_last = numExecutions >= TRAINING_NUM_DECISIONS;
@@ -396,7 +409,7 @@ let realistic_training_trials = {
 					
 					return !is_last;
 				}
-			}
+			})
         ],
         repetitions: 3
     }
@@ -504,7 +517,7 @@ let quiz_round = {
 }
 
 let quiz_timeline = { 
-    timeline: [quiz_round],
+    timeline: [createNodeWithTrial(quiz_round)],
     loop_function: function() { 
         // return true when it should loop 
         let data = JSON.parse(getDataAtIndex(jsPsych.progress().current_trial_global-1).responses);
@@ -524,7 +537,7 @@ let quiz_timeline = {
 
         let redo_questions = correctness.includes(false);
         if (redo_questions) attempts_quiz++;
-        if (attempts_quiz > 2) failedAttentionCheck = true; s
+        if (attempts_quiz >= 2) failedAttentionCheck = true; s
 
         return redo_questions;
     },
